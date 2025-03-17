042bef2d1227396ae47146957d488d4840733793
Baptiste <74823118+baptisteCanac@users.noreply.github.com>
2025-03-18 00:17:08 +0100

    déplacement définition commande
    déplacement de la création de la variable contenant la commande en dehors de la boucle pour fiabiliser le temps

--- crab_time/src/main.rs
+++ crab_time/src/main.rs
@@ -1,16 +1,15 @@
 use std::io;
 use std::process::Command;
-use std::time::Instant;
-use std::time::Duration;
+use std::time::{Instant, Duration};
 
-fn ask_entry(question: &str) -> String{
+fn ask_entry(question: &str) -> String {
     let mut input = String::new(); // variable qui stocke l'entrée utilisateur
 
     println!("{}", question);
 
     io::stdin().read_line(&mut input)
         .expect("Echec de la lecture de l'entrée");
-    
+
     input.trim().to_string() // retourne en String
 }
 
@@ -25,45 +24,38 @@ fn ask_int(question: &str) -> i32 {
 }
 
 fn main() {
-    /*
-    Ce programme analyse une commande
-    Il demande en entrée la commande à tester et le nombre de tests que l'on doit faire
-    Elle renvoit le temps que la commande met pour s'executer
-    */
+    // Demande la commande à analyser et le nombre de tests
+    let command: String = ask_entry("Entrez la commande à analyser: ");
+    let nombre_tests: i32 = ask_int("Combien de lancement faire (Plus le nombre de test sera élevé, plus les vérifications seront précises): ");
 
-    /*
-    Demande la commande à analyser et le nombre de tests
-    Le nombre de tests est codé sur 32 bits pour permettre un énorme nombre de tests
-    */
-    let command:String = ask_entry("Entrez la commande à analyser: ");
-    let nombre_tests:i32 = ask_int("Combien de lancement faire (Plus le nombre de test sera élevé, plus les vérifications seront précises): ");
+    let mut total_execution: Vec<Duration> = Vec::new(); // Créer tableau de taille variable
 
-    let mut total_execution: Vec<Duration> = Vec::new(); // créer tableau de taille variable
+    // Crée l'objet Command avant la boucle
+    let mut command_exec = Command::new("sh");
 
-    let mut i:i32 = 0;
-    while i <= nombre_tests{
-        // démare le chrono
+    // Ajoute les arguments à l'objet Command
+    command_exec.arg("-c").arg(&command);
+
+    let mut i: i32 = 0;
+    while i < nombre_tests {
+        // Démarre le chrono
         let start = Instant::now();
 
-        let exec = Command::new("sh")
-            .arg("-c")
-            .arg(&command)
-            .output()
-            .expect("Echec de la commande veuillez vérifier votre input");
-        
+        // Exécute la commande
+        let _ = command_exec.output().expect("Echec de la commande veuillez vérifier votre input");
+
+        // Arrête le chrono
+        let duration = start.elapsed();
+
+        // Incrémente le nombre de tests
         i += 1;
 
-        // arrete le chrono
-        let duration = start.elapsed();
-        total_execution.push(duration);
+        // Ajoute la durée d'exécution dans le tableau après 50 tests
+        if i > 50 {
+            total_execution.push(duration);
+        }
     }
 
+    // Affiche les durées d'exécution
     println!("{:?}", total_execution);
-
-    /*println!(
-        "Temps total: {}s {}ms ({}µs)",
-        duration.as_secs(),
-        duration.subsec_millis(),
-        duration.subsec_micros()
-    );*/
 }
